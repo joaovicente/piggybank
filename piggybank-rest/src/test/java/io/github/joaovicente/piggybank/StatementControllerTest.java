67dd28921148aeded279485a30473376a55d932e
Joao Vicente <joao.diogo.vicente@gmail.com>
2018-09-18 18:09:23 +0100

    Organized imports

--- piggybank-rest/src/test/java/io/github/joaovicente/piggybank/StatementControllerTest.java
+++ piggybank-rest/src/test/java/io/github/joaovicente/piggybank/StatementControllerTest.java
@@ -2,14 +2,7 @@ package io.github.joaovicente.piggybank;
 
 import io.github.joaovicente.piggybank.dto.StatementDto;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
-import io.restassured.specification.RequestSpecification;
 import lombok.extern.java.Log;
-import org.apache.http.HttpStatus;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,9 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static io.restassured.RestAssured.*;
-import static io.restassured.path.json.JsonPath.from;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsNull.notNullValue;
