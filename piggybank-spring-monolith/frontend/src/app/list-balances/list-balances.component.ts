639de4cb9443521072fed290a4a90321dc8d00c0
Joao Vicente <joao.diogo.vicente@gmail.com>
2018-12-22 15:24:40 +0000

    added initial components and services

---
+++ piggybank-spring-monolith/frontend/src/app/list-balances/list-balances.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-balances',
+  templateUrl: './list-balances.component.html',
+  styleUrls: ['./list-balances.component.css']
+})
+export class ListBalancesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
